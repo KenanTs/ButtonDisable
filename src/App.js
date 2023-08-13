697ef4a5edc91cb0a64a1e3a854a5a588cfecc7b
Kenan Tasdemir <bonox00@gmail.com>
2023-08-13 15:57:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // Count from without
+  const [count, _setCountValue] = useState(3);
+
+  function setCount(value) {
+    localStorage.setItem('count', JSON.stringify(value))
+    return _setCountValue(value)
+  }
+
+  // Disable-Enable Button
+  const [disabled, setDisabled] = useState(false);
+
+  // Set to 5 sec.
+  const handleTimer = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+      setDisabled(true);
+      const timeout = setTimeout(() => {
+        setDisabled(false);
+        setCount(3);
+      }, 5000);
+      return () => clearTimeout(timeout);
+    }
+  }
+
+  // useEffect for Disable Button (5 sec.)
+  useEffect(() => {
+    const storedcound = localStorage.getItem('count');
+    //console.log('storedcount: ', storedcound);
+    if (storedcound) {
+      setCount(parseInt(storedcound));
+      if (storedcound === '0') {
+        setDisabled(true);
+        const timeout = setTimeout(() => {
+          setDisabled(false);
+          setCount(3);
+        }, 5000);
+        return () => clearTimeout(timeout);
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <button
+        onClick={handleTimer}
+        disabled={disabled}
+      >
+        Test Button {count} / 3
+      </button>
+
+    </>
   );
 }
 
